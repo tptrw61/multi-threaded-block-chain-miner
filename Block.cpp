533b9c41ca44ac83795731377db34e7ad4c86689
Tim <tptrw.61@gmail.com>
2018-12-31 12:10:32 -0700

    initializing Block class

---
+++ Block.cpp
@@ -0,0 +1,123 @@
+#include "Block.hpp"
+
+std::hash<std::string> Block::hasher;
+
+Block::Block() {
+	this->id = 1;
+	this->difficulty = DEFAULT_DIFFICULTY;
+	this->previousHash = 0;
+	this->solvedHash = 0;
+	this->nonce = 0;
+	this->timeCreated = clock();
+	this->timeSolved = 0;
+	this->threshold = DIFFICULTY_VALUES[DEFAULT_DIFFICULTY];
+	this->nSol = 0;
+};
+
+Block::Block(unsigned id, size_t previousHash, unsigned difficulty) {
+	this->id = id;
+	this->difficulty = difficulty <= 16 ? difficulty : DEFAULT_DIFFICULTY;
+	this->previousHash = previousHash;
+	this->solvedHash = 0;
+	this->nonce = 0;
+	this->timeCreated = clock();
+	this->timeSolved = 0;
+	this->threshold = DIFFICULTY_VALUES[difficulty];
+	this->nSol = 0;
+}
+Block::Block(unsigned id, size_t previousHash, size_t solvedHash, size_t nonce, clock_t timeCreated, clock_t timeSolved, unsigned difficulty) {
+	this->id = id;
+	this->previousHash = previousHash;
+	this->solvedHash = solvedHash;
+	this->nonce = nonce;
+	this->timeCreated = timeCreated;
+	this->timeSolved = timeSolved;
+	this->difficulty = difficulty;
+	this->threshold = DIFFICULTY_VALUES[difficulty];
+	this->nSol = 1;
+};
+
+bool Block::isSolved() const {
+	return timeSolved || nSol;
+};
+
+bool Block::tryNonce(size_t nonce) {
+	size_t attempt = hasher(std::to_string(this->previousHash) + std::to_string(nonce));
+	if (attempt > this->threshold || this->nSol)
+		return 0;
+	if (timeSolved)
+		return 0;
+	this->solvedHash = attempt;
+	this->nonce = nonce;
+	this->timeSolved = clock();
+	return 1;
+};
+
+bool Block::tryNonce(size_t nonce) const {
+	size_t attempt = hasher(std::to_string(this->previousHash) + std::to_string(nonce));
+	if (attempt > this->threshold || this->nSol)
+		return 0;
+	if (timeSolved)
+		return 0;
+	return 1;
+};
+
+
+bool Block::operator==(Block b) const { return this->id == b.id && this->nonce == b.nonce; };
+bool Block::operator!=(Block b) const { return this->id != b.id || this->nonce != b.nonce; };
+bool Block::operator>=(Block b) const { return this->id >= b.id ? (this->nonce >= b.nonce) : false; };
+bool Block::operator<=(Block b) const { return this->id <= b.id ? (this->nonce <= b.nonce) : false; };
+bool Block::operator>(Block b) const { return this->id > b.id ? (this->nonce > b.nonce) : false; };
+bool Block::operator<(Block b) const { return this->id < b.id ? (this->nonce < b.nonce) : false; };
+
+void Block::setNoSolution() {
+	if (timeSolved)
+		return;
+	this->solvedHash = hasher(std::to_string(previousHash));
+	this->timeSolved = clock();
+	this->nSol = 1;
+};
+
+unsigned Block::getId() const { return this->id; };
+unsigned Block::getDifficulty() const { return this->difficulty; };
+size_t Block::getPreviousHash() const { return this->previousHash; };
+size_t Block::getSolvedHash() const { return this->solvedHash; };
+size_t Block::getNonce() const { return this->nonce; };
+clock_t Block::getTimeCreated() const { return this->timeCreated; };
+clock_t Block::getTimeSolved() const { return this->timeSolved; };
+bool Block::hasNoSolution() const { return this->nSol; };
+
+bool Block::editDifficulty(unsigned difficulty) {
+	if (difficulty > 16)
+		return 0;
+	this->difficulty = difficulty;
+	return 1;
+};
+
+std::string Block::toString(bool abridged) const {
+	std::string s = "";
+	if (abridged) {
+		s += "id: " + std::to_string(this->id) + "; ";
+		s += "nonce: " + std::to_string(this->nonce);
+	} else {
+		s += "id            : " + std::to_string(this->id) + "\n";
+		s += "previous hash : " + std::to_string(this->previousHash) + "\n";
+		s += "solved hash   : " + std::to_string(this->solvedHash) + "\n";
+		s += "nonce         : " + std::to_string(this->nonce) + "\n";
+		s += "time created  : " + std::to_string(this->timeCreated) + "\n";
+		s += "time solved   : " + std::to_string(this->timeSolved);
+	}
+	return s;
+};
+
+
+
+char mineBlock(Block &block, size_t nonceStart, int nonceIncrement, size_t nonceEnd) {
+	size_t i;
+	for (i = nonceStart; i <= nonceEnd; i++)
+		if (block.isSolved())
+			return 'd';
+		else if (block.tryNonce(i))
+			break;
+	return i == block.getNonce() ? 's' : 'n';
+};
---
+++ Block.hpp
@@ -0,0 +1,154 @@
+#pragma once
+
+#include <ctime>
+#include <functional>
+#include <string>
+
+#define DEFAULT_DIFFICULTY 3
+
+const size_t DIFFICULTY_VALUES[] = {
+	0xffffffffffffffff,
+	0x0fffffffffffffff,
+	0x00ffffffffffffff,
+	0x000fffffffffffff,
+	0x0000ffffffffffff,
+	0x00000fffffffffff,
+	0x000000ffffffffff,
+	0x0000000fffffffff,
+	0x00000000ffffffff,
+	0x000000000fffffff,
+	0x0000000000ffffff,
+	0x00000000000fffff,
+	0x000000000000ffff,
+	0x0000000000000fff,
+	0x00000000000000ff,
+	0x000000000000000f,
+	0x0000000000000000
+};
+
+
+
+class Block {
+
+	unsigned id;
+	unsigned difficulty;
+	size_t previousHash;
+	size_t solvedHash;
+	size_t nonce;
+	clock_t timeCreated;
+	clock_t timeSolved;
+	size_t threshold;
+	bool nSol; //no solutions, true only if the hash is impossible at current difficulty
+
+	static std::hash<std::string> hasher;
+
+public:
+
+	Block();
+	Block(unsigned id, size_t previousHash, unsigned difficulty = DEFAULT_DIFFICULTY);
+	Block(unsigned id, size_t previousHash, size_t solvedHash, size_t nonce, clock_t timeCreated, clock_t timeSolved, unsigned difficulty = DEFAULT_DIFFICULTY);
+
+	bool isSolved() const;
+	bool tryNonce(size_t nonce);
+	bool tryNonce(size_t nonce) const;
+
+	bool operator==(Block) const;
+	bool operator!=(Block) const;
+	bool operator>=(Block) const;
+	bool operator<=(Block) const;
+	bool operator>(Block) const;
+	bool operator<(Block) const;
+
+	//if the miner has checked all possible nonces andthere is no solution at this difficulty
+	//sets timeSolved, sets solvedHash to hash of previousHash, sets nonce to 0, and sets nSol to true
+	void setNoSolution();
+
+	unsigned getId() const;
+	unsigned getDifficulty() const;
+	size_t getPreviousHash() const;
+	size_t getSolvedHash() const;
+	size_t getNonce() const;
+	clock_t getTimeCreated() const;
+	clock_t getTimeSolved() const;
+	bool hasNoSolution() const;
+
+	bool editDifficulty(unsigned difficulty);
+
+	std::string toString(bool abridged = true) const;
+
+};
+
+char mineBlock(Block &block, size_t nonceStart = 0, int nonceIncrement = 1, size_t nonceEnd = -1);
+
+#ifdef COMPARE_BLOCK_STRUCT
+class CompareBlock {
+public:
+
+	static const int COMPARE_DEFAULT = 0;
+	static const int COMPARE_ID = 1;
+	static const int COMPARE_NONCE = 2;
+
+	int mode = COMPARE_DEFAULT;
+
+	bool eq(Block b1, Block b2) {
+		if (mode == COMPARE_DEFAULT)
+			return b1 == b2;
+		if (mode == COMPARE_ID)
+			return b1.getId() == b2.getId();
+		if (mode == COMPARE_NONCE)
+			return b1.getNonce() == b2.getNonce();
+		return false;
+	};
+
+	bool ne(Block b1, Block b2) {
+		if (mode == COMPARE_DEFAULT)
+			return b1 != b2;
+		if (mode == COMPARE_ID)
+			return b1.getId() != b2.getId();
+		if (mode == COMPARE_NONCE)
+			return b1.getNonce() != b2.getNonce();
+		return false;
+	};
+
+	bool ge(Block b1, Block b2) {
+		if (mode == COMPARE_DEFAULT)
+			return b1 >= b2;
+		if (mode == COMPARE_ID)
+			return b1.getId() >= b2.getId();
+		if (mode == COMPARE_NONCE)
+			return b1.getNonce() >= b2.getNonce();
+		return false;
+	};
+
+	bool le(Block b1, Block b2) {
+		if (mode == COMPARE_DEFAULT)
+			return b1 <= b2;
+		if (mode == COMPARE_ID)
+			return b1.getId() <= b2.getId();
+		if (mode == COMPARE_NONCE)
+			return b1.getNonce() <= b2.getNonce();
+		return false;
+	};
+
+	bool gt(Block b1, Block b2) {
+		if (mode == COMPARE_DEFAULT)
+			return b1 > b2;
+		if (mode == COMPARE_ID)
+			return b1.getId() > b2.getId();
+		if (mode == COMPARE_NONCE)
+			return b1.getNonce() > b2.getNonce();
+		return false;
+	};
+
+	bool lt(Block b1, Block b2) {
+		if (mode == COMPARE_DEFAULT)
+			return b1 < b2;
+		if (mode == COMPARE_ID)
+			return b1.getId() < b2.getId();
+		if (mode == COMPARE_NONCE)
+			return b1.getNonce() < b2.getNonce();
+		return false;
+	};
+
+};
+#endif
