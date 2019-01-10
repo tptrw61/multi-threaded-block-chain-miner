eccbaeec341aade73cf1e879152b87c57aae04dd
Tim Williams <tptrw.61@gmail.com>
2019-01-09 21:45:56 -0700

    removing commented out code

--- timer.hpp
+++ timer.hpp
@@ -188,37 +188,3 @@ std::string Timer::toString(size_t dur, TimeResolution resolution, TimeResolutio
     return str;
 }
 
-/*
-#include <stdio.h>
-const int SECOND_STR_LEN = 6;
-const int MINUTE_STR_LEN = 9;
-const int HOUR_STR_LEN = 12;
-const int MAX_STR_LEN = 25;
-const int TIME_STR_LEN = 32;
-int msToTimeString(char * str, size_t t) {
-	if (str == NULL)
-		return -1;
-	size_t d;
-	unsigned h, m, s, ms;
-	ms = t % 1000; t /= 1000;
-	s = t % 60; t /= 60;
-	m = t % 60; t /= 60;
-	h = t % 24; t /= 24;
-	d = t;
-	sprintf(str, "%zu:%02u:%02u:%02u.%03u", d, h, m, s, ms);
-	int i, c;
-	for (i = c = 0; 1; i++) {
-		if (str[i] == ':' || str[i] == '.') {
-			c++;
-			if (c == 1 && str[i - 1] != '0')
-				return 0;
-			if (c == 2 && (str[i - 1] != '0' || str[i - 2] != '0'))
-				return 2;
-			if (c == 3 && (str[i - 1] != '0' || str[i - 2] != '0'))
-				return 5;
-			if (c == 4)
-				return 8;
-		}
-	}
-};
-*/
